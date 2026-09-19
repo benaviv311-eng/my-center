eaf72c46ad6e89d35e20f36e1208ad74800634a1
benaviv311-eng <benaviv311@gmail.com>
2026-09-19 14:50:55 +0300

    Add TeamScore PWA shell

---
+++ team-score/sw.js
@@ -0,0 +1,5 @@
+const CACHE='teamscore-pilot-v17';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./t1.txt','./t2.txt','./t3.txt','./t4.txt','./b1.txt','./b2.txt','./b3.txt','./b4.txt'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request).then(r=>{const y=r.clone();caches.open(CACHE).then(c=>c.put(e.request,y));return r})))});
