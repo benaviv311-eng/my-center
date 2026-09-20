6f7b1b8588990b31d5482dafaaaba176491e99b9
benaviv311-eng <benaviv311@gmail.com>
2026-09-20 07:41:18 +0300

    TeamScore: activate 24-background rotation v19

--- team-score/sw.js
+++ team-score/sw.js
@@ -1,4 +1,4 @@
-const CACHE='teamscore-pilot-v18';
+const CACHE='teamscore-pilot-v19';
 const ASSETS=[
   './','./index.html','./manifest.webmanifest','./icon.svg',
   './t1.txt','./t2.txt','./t3.txt','./t4.txt',
