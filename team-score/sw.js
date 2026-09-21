8bfdddee7b438ed2fbd9d1e993679c24385a9dea
benaviv311-eng <benaviv311@gmail.com>
2026-09-21 21:43:16 +0300

    Refresh TeamScore cache v25

--- team-score/sw.js
+++ team-score/sw.js
@@ -1,4 +1,4 @@
-const CACHE='teamscore-pilot-v24';
+const CACHE='teamscore-pilot-v25';
 const ASSETS=[
   './','./index.html','./manifest.webmanifest','./icon.svg',
   './t1.txt','./t2.txt','./t3.txt','./t4.txt',
