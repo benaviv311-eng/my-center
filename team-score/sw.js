7c7b04f5c3952368263dc71902182fcbf10b7952
benaviv311-eng <benaviv311@gmail.com>
2026-09-21 21:09:36 +0300

    Refresh TeamScore cache v22

--- team-score/sw.js
+++ team-score/sw.js
@@ -1,4 +1,4 @@
-const CACHE='teamscore-pilot-v20';
+const CACHE='teamscore-pilot-v22';
 const ASSETS=[
   './','./index.html','./manifest.webmanifest','./icon.svg',
   './t1.txt','./t2.txt','./t3.txt','./t4.txt',
