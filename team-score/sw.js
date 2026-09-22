43060afd8bb42963643874cf6ba2b77518d8931a
benaviv311-eng <benaviv311@gmail.com>
2026-09-22 06:40:01 +0300

    TeamScore: refresh cache for sharper backgrounds v27

--- team-score/sw.js
+++ team-score/sw.js
@@ -1,4 +1,4 @@
-const CACHE='teamscore-pilot-v26';
+const CACHE='teamscore-pilot-v27';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./t1.txt','./t2.txt','./t3.txt','./t4.txt','./b1.txt','./b2.txt','./b3.txt','./b4.txt'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
